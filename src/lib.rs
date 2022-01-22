96458993b3b6c077d18a35cf0c15ea04e559bc26
Kotaro Abe <abekoh.bcky@gmail.com>
2022-01-22 14:31:15 +0900

    update resolver

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,28 @@ trait Resolver {
     fn add_hint(results: Vec<Hint>);
     fn guess() -> Vec<String>;
 }
+
+struct SimpleResolver {
+    dict_words: Vec<String>;
+}
+
+impl SimpleResolver {
+    fn new(dict_words: Vec<&str>) -> SimpleResolver {
+        SimpleResolver {
+            dict_words: dict_words.iter()
+                .map(|word| {
+                    word.to_string()
+                }).collect()
+        }
+    }
+}
+
+impl Resolver for SimpleResolver {
+    fn add_hint(results: Vec<Hint>) {
+        todo!()
+    }
+
+    fn guess() -> Vec<String> {
+        todo!()
+    }
+}
