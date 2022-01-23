82bb423024915e1ef8a3562053397b1cb913ad59
Kotaro Abe <abekoh.bcky@gmail.com>
2022-01-23 17:31:12 +0900

    set width

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use wordle_resolver::{Hint, Resolver, SimpleResolver, Spot};
 
 fn main() {
-    let mut resolver = SimpleResolver::new(, vec!["story", "hello"]);
+    let mut resolver = SimpleResolver::new(5, vec!["story", "hello"]);
     resolver.add_hint(vec![Hint::new('a', Spot::None())]);
     resolver.add_hint(vec![Hint::new('t', Spot::At(vec![1]))]);
     for guessed in resolver.guess() {
