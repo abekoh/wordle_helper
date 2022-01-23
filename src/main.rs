2a057a411cc2980fe9025bcb3b58658fe095dddc
Kotaro Abe <abekoh.bcky@gmail.com>
2022-01-23 17:19:42 +0900

    fix main

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use wordle_resolver::{Hint, Resolver, SimpleResolver, Spot};
 
 fn main() {
     let mut resolver = SimpleResolver::new(vec!["story", "hello"]);
-    resolver.add_hint(&vec![Hint::new('a', Spot::None())], );
-    resolver.add_hint(&vec![Hint::new('t', Spot::At(vec![1]))], );
+    resolver.add_hint(vec![Hint::new('a', Spot::None())]);
+    resolver.add_hint(vec![Hint::new('t', Spot::At(vec![1]))]);
     for guessed in resolver.guess() {
         println!("{}", guessed);
     }
