8c18722d158d643cdc4a9effebb458704afd3bff
Kotaro Abe <abekoh.bcky@gmail.com>
2022-01-28 23:18:45 +0900

    refactoring (#13)

--- src/simple.rs
+++ src/simple.rs
@@ -86,15 +86,15 @@ mod tests {
 
         #[test]
         fn asset_fields() {
-            let actual = SimpleSolver::new(5, &vec!["hello".to_string(), "early".to_string()]);
-            assert_eq!(actual.width, 5);
-            assert_eq!(actual.dict_words, vec!["hello", "early"]);
+            let target = SimpleSolver::new(5, &vec!["hello".to_string(), "early".to_string()]);
+            assert_eq!(target.width, 5);
+            assert_eq!(target.dict_words, vec!["hello", "early"]);
         }
 
         #[test]
         fn filter_word_only_length_is_5() {
-            let actual = SimpleSolver::new(5, &vec!["hello".to_string(), "dog".to_string(), "in".to_string(), "early".to_string(), "difference".to_string()]);
-            assert_eq!(actual.dict_words, vec!["hello", "early"]);
+            let target = SimpleSolver::new(5, &vec!["hello".to_string(), "dog".to_string(), "in".to_string(), "early".to_string(), "difference".to_string()]);
+            assert_eq!(target.dict_words, vec!["hello", "early"]);
         }
     }
 
@@ -109,9 +109,9 @@ mod tests {
 
         #[test]
         fn remove_word() {
-            let mut actual = SimpleSolver::new(5, &preset_words());
-            actual.add_hint("hello", &vec![]);
-            assert_eq!(actual.guess(), &vec![String::from("asset"), String::from("early")]);
+            let mut target = SimpleSolver::new(5, &preset_words());
+            target.add_hint("hello", &vec![]);
+            assert_eq!(target.guess(), &vec![String::from("asset"), String::from("early")]);
         }
 
         #[cfg(test)]
@@ -120,16 +120,16 @@ mod tests {
 
             #[test]
             fn a() {
-                let mut actual = SimpleSolver::new(5, &preset_words());
-                actual.add_hint("dummy", &vec![Hint { letter: 'a', spot: Spot::None() }]);
-                assert_eq!(actual.guess(), &vec![String::from("hello")]);
+                let mut target = SimpleSolver::new(5, &preset_words());
+                target.add_hint("dummy", &vec![Hint { letter: 'a', spot: Spot::None() }]);
+                assert_eq!(target.guess(), &vec![String::from("hello")]);
             }
 
             #[test]
             fn l() {
-                let mut actual = SimpleSolver::new(5, &preset_words());
-                actual.add_hint("dummy", &vec![Hint { letter: 'l', spot: Spot::None() }]);
-                assert_eq!(actual.guess(), &vec![String::from("asset")]);
+                let mut target = SimpleSolver::new(5, &preset_words());
+                target.add_hint("dummy", &vec![Hint { letter: 'l', spot: Spot::None() }]);
+                assert_eq!(target.guess(), &vec![String::from("asset")]);
             }
         }
 
@@ -139,16 +139,16 @@ mod tests {
 
             #[test]
             fn l_2() {
-                let mut actual = SimpleSolver::new(5, &preset_words());
-                actual.add_hint("dummy", &vec![Hint { letter: 'l', spot: Spot::InWithout(2) }]);
-                assert_eq!(actual.guess(), &vec![String::from("early")]);
+                let mut target = SimpleSolver::new(5, &preset_words());
+                target.add_hint("dummy", &vec![Hint { letter: 'l', spot: Spot::InWithout(2) }]);
+                assert_eq!(target.guess(), &vec![String::from("early")]);
             }
 
             #[test]
             fn e_0() {
-                let mut actual = SimpleSolver::new(5, &preset_words());
-                actual.add_hint("dummy", &vec![Hint { letter: 'e', spot: Spot::InWithout(0) }]);
-                assert_eq!(actual.guess(), &vec![String::from("hello"), String::from("asset")]);
+                let mut target = SimpleSolver::new(5, &preset_words());
+                target.add_hint("dummy", &vec![Hint { letter: 'e', spot: Spot::InWithout(0) }]);
+                assert_eq!(target.guess(), &vec![String::from("hello"), String::from("asset")]);
             }
         }
 
@@ -159,9 +159,9 @@ mod tests {
 
             #[test]
             fn t_4() {
-                let mut actual = SimpleSolver::new(5, &preset_words());
-                actual.add_hint("dummy", &vec![Hint { letter: 't', spot: Spot::At(4) }]);
-                assert_eq!(actual.guess(), &vec![String::from("asset")]);
+                let mut target = SimpleSolver::new(5, &preset_words());
+                target.add_hint("dummy", &vec![Hint { letter: 't', spot: Spot::At(4) }]);
+                assert_eq!(target.guess(), &vec![String::from("asset")]);
             }
         }
 
@@ -171,7 +171,7 @@ mod tests {
 
             #[test]
             fn multiple_1() {
-                let mut actual = SimpleSolver::new(5, &vec![
+                let mut target = SimpleSolver::new(5, &vec![
                     "hello".to_string(),
                     "early".to_string(),
                     "asset".to_string(),
@@ -179,22 +179,22 @@ mod tests {
                     "heard".to_string(),
                     "spice".to_string(),
                 ]);
-                actual.add_hint("bound", &vec![
+                target.add_hint("bound", &vec![
                     Hint::new('b', Spot::None()),
                     Hint::new('o', Spot::None()),
                     Hint::new('u', Spot::None()),
                     Hint::new('n', Spot::None()),
                     Hint::new('d', Spot::None()),
                 ]);
-                assert_eq!(actual.guess(), &vec![String::from("early"), String::from("asset"), String::from("spice")]);
-                actual.add_hint("spice", &vec![
+                assert_eq!(target.guess(), &vec![String::from("early"), String::from("asset"), String::from("spice")]);
+                target.add_hint("spice", &vec![
                     Hint::new('s', Spot::InWithout(0)),
                     Hint::new('p', Spot::None()),
                     Hint::new('i', Spot::None()),
                     Hint::new('c', Spot::None()),
                     Hint::new('e', Spot::InWithout(4)),
                 ]);
-                assert_eq!(actual.guess(), &vec![String::from("asset")]);
+                assert_eq!(target.guess(), &vec![String::from("asset")]);
             }
         }
     }
