7d7ae80d6e8c9ff288d741802b2e0059694696d1
Kotaro Abe <abekoh.bcky@gmail.com>
2022-01-30 20:11:40 +0900

    upgrade ui v2 (#31)
    * input
    
    * fix input
    
    * add error
    
    * fix format
    
    * add gameover
    
    * fix when gameover
    
    * add all_at
    
    * add congrats pattern
    
    * fix line
    
    * fix println
    
    * show as caps
    
    * add plain
    
    * preview
    
    * use pretty
    
    * add test
    
    * use self
    
    * add word_width
    
    * fix
    
    * update pretty
    
    * fix
    
    * add answer_length
    
    * fix pretty
    
    * update preview
    
    * fix with clippy
    
    * print info
    
    * use validate
    
    * fix printing
    
    * update hint sample
    
    * fix prompt
    
    * refactoring
    
    * use colorize
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * sample
    
    * tmp
    
    * fix showing example

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,25 @@
 use std::iter::zip;
 
-use ansi_term::{Colour, Style};
+use ansi_term::{ANSIGenericString, Colour, Style};
 use ansi_term::Color::{RGB, White};
+use ansi_term::Colour::Cyan;
 use clap::Parser;
 use dialoguer::{Confirm, FuzzySelect, Input, Select};
 use dialoguer::theme::ColorfulTheme;
 use num_format::{Locale, ToFormattedString};
 
 use wordle_solver::{Dictionary, Hint, Solver, Spot};
-use wordle_solver::txt::TxtDictionary;
 use wordle_solver::simple::SimpleSolver;
+use wordle_solver::txt::TxtDictionary;
 
 #[derive(Parser)]
 #[clap(version, about, long_about = None)]
 struct Config {
-    #[clap(short, long, default_value_t = 5, help = "length or word")]
+    #[clap(short, long, default_value_t = 5, help = "length of one word")]
     word_length: usize,
+
+    #[clap(short, long, default_value_t = 6, help = "how many you can answer")]
+    answer_length: usize,
 
     #[clap(short, long, hide_default_value = true, default_value = "", help = "dictionary path")]
     dict_path: String,
@@ -34,9 +38,11 @@ fn main() {
             std::process::exit(1);
         }
     });
+    println!("{}", Cyan.paint(format!("word length: {}", config.word_length)));
+    println!("{}", Cyan.paint(format!("number of answer you can guess: {}", config.answer_length)));
 
     let mut solver: Box<dyn Solver> = Box::new(SimpleSolver::new(config.word_length, &dictionary.extract_words(config.word_length)));
-    let mut states: InputStates = InputStates::new();
+    let mut states: InputStates = InputStates::new(config.word_length, config.answer_length);
 
     loop {
         let remained_words_length = solver.remained_words_length();
@@ -48,6 +54,7 @@ fn main() {
 
         loop {
             println!();
+            println!("{}", Style::new().bold().paint(format!("ROUND {}/{}", states.round_count + 1, config.answer_length)));
             println!("There are {} words are remained.", remained_words_length.to_formatted_string(&Locale::en));
 
             let mut state = InputState::new(config.word_length);
@@ -76,6 +83,14 @@ fn main() {
                 1 => {
                     let input: String = Input::with_theme(&ColorfulTheme::default())
                         .with_prompt("Guess")
+                        .validate_with({
+                            move |input: &String| -> Result<(), &str> {
+                                if input.len() != config.word_length {
+                                    return Err("invalid length");
+                                }
+                                Ok(())
+                            }
+                        })
                         .interact_text()
                         .unwrap();
                     state.add_word(&input).unwrap();
@@ -86,8 +101,11 @@ fn main() {
                 }
             }
 
+            println!("Input answer like this:");
+            println!("{}", states.preview(&state).unwrap());
+
             if Confirm::with_theme(&ColorfulTheme::default())
-                .with_prompt("Correct?")
+                .with_prompt(format!("Is \"{}\" corrected answer?", state.word().unwrap().to_uppercase()))
                 .interact()
                 .unwrap()
             {
@@ -95,6 +113,34 @@ fn main() {
                 println!("{}\n", Style::new().bold().paint("Congratulation!!"));
                 println!("{}", states.preview(&state).unwrap());
                 std::process::exit(0);
+            }
+
+            if states.is_final_round() {
+                println!("{}\n", Style::new().bold().paint(format!("X/{} GAME OVER!!", config.answer_length)));
+                println!("{}", states.preview(&state).unwrap());
+                std::process::exit(1);
+            }
+            states.increment_round();
+
+            println!(r#"Input {} numbers in order as hint;
+· nowhere   -> {}
+· somewhere -> {}
+· just      -> {}"#,
+                     config.word_length,
+                     colorize(&HintInputType::Nowhere, "0"),
+                     colorize(&HintInputType::Somewhere, "1"),
+                     colorize(&HintInputType::Just, "2"),
+            );
+
+            if states.round_count == 1 {
+                println!("{}{}{}{}{}{}",
+                         Style::new().fg(RGB(128, 128, 128)).paint("e.g.) SOLVE + 10221 => "),
+                         colorize(&HintInputType::Somewhere, "S"),
+                         colorize(&HintInputType::Nowhere, "O"),
+                         colorize(&HintInputType::Just, "L"),
+                         colorize(&HintInputType::Just, "V"),
+                         colorize(&HintInputType::Somewhere, "E"),
+                );
             }
 
             let hint_input = Input::with_theme(&ColorfulTheme::default())
@@ -142,16 +188,30 @@ fn main() {
     }
 }
 
+const BACK_GREEN: Colour = RGB(83, 141, 78);
+const BACK_YELLOW: Colour = RGB(180, 159, 58);
+const BACK_GRAY: Colour = RGB(58, 58, 60);
+
+enum HintInputType {
+    Nowhere,
+    Somewhere,
+    Just,
+}
+
+fn colorize<'a>(hint_type: &'a HintInputType, text: &'a str) -> ANSIGenericString<'a, str> {
+    return match hint_type {
+        HintInputType::Nowhere => Style::new().on(BACK_GRAY).fg(White).bold().paint(text.to_string()),
+        HintInputType::Somewhere => Style::new().on(BACK_YELLOW).fg(White).bold().paint(text.to_string()),
+        HintInputType::Just => Style::new().on(BACK_GREEN).fg(White).bold().paint(text.to_string()),
+    };
+}
+
 struct InputState {
     width: usize,
     word: Option<String>,
     hint: Vec<Hint>,
     is_correct: bool,
 }
-
-const BACK_GREEN: Colour = RGB(83, 141, 78);
-const BACK_YELLOW: Colour = RGB(180, 159, 58);
-const BACK_GRAY: Colour = RGB(58, 58, 60);
 
 impl InputState {
     pub fn new(width: usize) -> Self {
@@ -199,8 +259,29 @@ impl InputState {
         Result::Ok(())
     }
 
+    pub fn word(&self) -> Option<&String> {
+        match &self.word {
+            Some(w) => Some(w),
+            None => None,
+        }
+    }
+
     pub fn correct(&mut self) {
         self.is_correct = true;
+    }
+
+    pub fn is_draft(&self) -> bool {
+        if self.is_correct {
+            return false;
+        }
+        self.word.is_some() && self.hint.is_empty()
+    }
+
+    pub fn plain(&self) -> Result<String, &'static str> {
+        if self.word.is_none() {
+            return Result::Err("word are empty");
+        }
+        Result::Ok(format!("{}", Style::new().fg(White).bold().paint(self.word.as_ref().unwrap().to_uppercase())))
     }
 
     pub fn colorized(&self) -> Result<String, &'static str> {
@@ -208,7 +289,7 @@ impl InputState {
             return Result::Err("word are empty");
         }
         if self.is_correct {
-            return Result::Ok(format!("{}", Style::new().on(BACK_GREEN).fg(White).bold().paint(self.word.as_ref().unwrap().to_uppercase())));
+            return Result::Ok(format!("{}", colorize(&HintInputType::Just, &self.word.as_ref().unwrap().to_uppercase())));
         }
         if self.hint.is_empty() {
             return Result::Err("hints are empty");
@@ -216,9 +297,9 @@ impl InputState {
         let mut chars: Vec<String> = Vec::new();
         for (c, hint) in zip(self.word.as_ref().unwrap().chars(), &self.hint) {
             let res = match hint.spot {
-                Spot::None() => format!("{}", Style::new().on(BACK_GRAY).fg(White).bold().paint(c.to_string().to_uppercase())),
-                Spot::InWithout(_) => format!("{}", Style::new().on(BACK_YELLOW).fg(White).bold().paint(c.to_string().to_uppercase())),
-                Spot::At(_) => format!("{}", Style::new().on(BACK_GREEN).fg(White).bold().paint(c.to_string().to_uppercase())),
+                Spot::None() => format!("{}", colorize(&HintInputType::Nowhere, &c.to_string().to_uppercase())),
+                Spot::InWithout(_) => format!("{}", colorize(&HintInputType::Somewhere, &c.to_string().to_uppercase())),
+                Spot::At(_) => format!("{}", colorize(&HintInputType::Just, &c.to_string().to_uppercase())),
             };
             chars.push(res);
         }
@@ -240,12 +321,15 @@ impl InputState {
 }
 
 struct InputStates {
+    word_width: usize,
+    answer_width: usize,
     states: Vec<InputState>,
+    pub round_count: i32,
 }
 
 impl InputStates {
-    pub fn new() -> Self {
-        InputStates { states: Vec::new() }
+    pub fn new(word_width: usize, answer_width: usize) -> Self {
+        InputStates { states: Vec::new(), word_width, answer_width, round_count: 0 }
     }
 
     pub fn add(&mut self, state: InputState) {
@@ -262,13 +346,42 @@ impl InputStates {
                 }
             }
         }
-        match staged_state.colorized() {
+
+        let staged_text = match staged_state.is_draft() {
+            true => staged_state.plain(),
+            false => staged_state.colorized(),
+        };
+        match staged_text {
             Ok(s) => results.push(s),
             Err(e) => {
                 return Err(e);
             }
         }
-        Ok(results.join("\n"))
+        Ok(InputStates::pretty_preview(self, &results))
+    }
+
+    fn pretty_preview(&self, word_strs: &[String]) -> String {
+        let header_footer: String = format!("+{}+", "-".repeat(self.word_width));
+
+        let mut results: Vec<String> = Vec::new();
+        results.push(header_footer.clone());
+        for i in 0..self.answer_width {
+            if i < word_strs.len() {
+                results.push(format!("|{}|", word_strs[i]));
+            } else {
+                results.push(format!("|{}|", " ".repeat(self.word_width)));
+            }
+        }
+        results.push(header_footer);
+        results.join("\n")
+    }
+
+    fn increment_round(&mut self) {
+        self.round_count += 1;
+    }
+
+    fn is_final_round(&self) -> bool {
+        (self.round_count + 1) == self.answer_width as i32
     }
 }
 
@@ -341,6 +454,27 @@ mod tests {
                 let actual = state.add_hint("00120");
                 assert!(actual.is_err());
             }
+        }
+    }
+
+    #[cfg(test)]
+    mod input_states {
+        use super::*;
+
+        #[test]
+        fn pretty_preview() {
+            let target = InputStates::new(5, 6);
+            let actual = target.pretty_preview(&["ABCDE".to_string(),
+                "FGHIJ".to_string(),
+                "KLMNO".to_string()]);
+            assert_eq!(actual, r#"+-----+
+|ABCDE|
+|FGHIJ|
+|KLMNO|
+|     |
+|     |
+|     |
++-----+"#)
         }
     }
 }
