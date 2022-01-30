86c4b5cdbd78afdd9a4a4299f341041865f45206
Kotaro Abe <abekoh.bcky@gmail.com>
2022-01-30 15:33:47 +0900

    fix loading

--- src/txt.rs
+++ src/txt.rs
@@ -1,7 +1,27 @@
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::env;
+use std::path::{Path, PathBuf};
 use crate::Dictionary;
+
+const DEFAULT_CACHE_DIR: &str = "wordle-solver";
+const DEFAULT_FILENAME: &str = "words_alpha.txt";
+
+fn default_dict_path() -> PathBuf {
+    let result = match env::var("XDG_CACHE_HOME") {
+        Ok(v) => Path::new(v.as_str()).join(DEFAULT_CACHE_DIR).join(DEFAULT_FILENAME),
+        Err(_) => match env::var("HOME") {
+            Ok(v) => {
+                Path::new(v.as_str()).join(".config").join(DEFAULT_CACHE_DIR).join(DEFAULT_FILENAME)
+            }
+            Err(_) => {
+                Path::new("/tmp").join(DEFAULT_CACHE_DIR).join(DEFAULT_FILENAME)
+            }
+        }
+    };
+    result
+}
 
 pub struct TxtDictionary {
     file: File,
@@ -9,7 +29,11 @@ pub struct TxtDictionary {
 
 impl TxtDictionary {
     pub fn new(path: &str) -> io::Result<Self> {
-        let file = File::open(path.to_string())?;
+        let mut path_buf: PathBuf = PathBuf::from(path);
+        if path == "" {
+            path_buf = default_dict_path()
+        }
+        let file = File::open(path_buf)?;
         Ok(TxtDictionary { file })
     }
 }
